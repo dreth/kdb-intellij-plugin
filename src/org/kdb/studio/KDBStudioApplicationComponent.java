44bdcc9f100f40d91d842fa73b60a45a913a2932
Andrii Borovyk <shupakabras@gmail.com>
2018-07-16 16:21:41 +0300

    Create View -> KDB+ Studio Toolbar and make it auto-selected at installation

--- src/org/kdb/studio/KDBStudioApplicationComponent.java
+++ src/org/kdb/studio/KDBStudioApplicationComponent.java
@@ -5,13 +5,16 @@ import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.Storage;
-import com.intellij.openapi.keymap.KeymapManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.kdb.studio.actions.ConnectionsBoxAction;
 import org.kdb.studio.actions.RunAllAction;
 import org.kdb.studio.actions.RunCodeAction;
+import org.kdb.studio.actions.ToolbarControlAction;
 import org.kdb.studio.db.ConnectionManager;
+import org.kdb.studio.ui.KDBToolbarUIManager;
+
+import java.util.Optional;
 
 @com.intellij.openapi.components.State(
         name = "KDBStudio",
@@ -20,9 +23,10 @@ import org.kdb.studio.db.ConnectionManager;
 )
 public class KDBStudioApplicationComponent implements ApplicationComponent, PersistentStateComponent<State> {
 
-    public static final String MAIN_TOOL_BAR = "MainToolBar";
 
     private DefaultActionGroup kdbActionGroup;
+
+    boolean initialEnabled = false;
 
     @Override
     public void initComponent() {
@@ -30,19 +34,18 @@ public class KDBStudioApplicationComponent implements ApplicationComponent, Pers
         RunCodeAction runCodeAction = new RunCodeAction(connectionManager);
         RunAllAction runAllAction = new RunAllAction(connectionManager);
 
-        kdbActionGroup = new DefaultActionGroup();
+        kdbActionGroup = new DefaultActionGroup("KDBToolbarActions", false);
         kdbActionGroup.add(new ConnectionsBoxAction(connectionManager));
         kdbActionGroup.add(runCodeAction);
         kdbActionGroup.add(runAllAction);
+        KDBToolbarUIManager.initInstance(kdbActionGroup, initialEnabled);
 
         ActionManager am = ActionManager.getInstance();
         am.registerAction("KDB_run_line", runCodeAction);
         am.registerAction("KDB_run_all", runAllAction);
 
-        DefaultActionGroup windowM = (DefaultActionGroup) am.getAction(MAIN_TOOL_BAR);
-
-        windowM.add(kdbActionGroup);
-
+        DefaultActionGroup uiToggleGroup = (DefaultActionGroup) am.getAction("UIToggleActions");
+        uiToggleGroup.add(new ToolbarControlAction());
     }
 
     @Override
@@ -61,11 +64,16 @@ public class KDBStudioApplicationComponent implements ApplicationComponent, Pers
 
     @Nullable
     public State getState() {
-        return State.create(ConnectionManager.getInstance());
+        return State.create(ConnectionManager.getInstance(), KDBToolbarUIManager.getInstance() != null ? KDBToolbarUIManager.getInstance().isVisible() : initialEnabled);
     }
 
     public void loadState(State state) {
         state.apply(ConnectionManager.getInstance());
+        if (KDBToolbarUIManager.getInstance() == null) {
+            initialEnabled = Optional.ofNullable(state.getToolbarEnabled()).orElse(true);
+        } else {
+            KDBToolbarUIManager.getInstance().setState(Optional.ofNullable(state.getToolbarEnabled()).orElse(true));
+        }
     }
 
 }
