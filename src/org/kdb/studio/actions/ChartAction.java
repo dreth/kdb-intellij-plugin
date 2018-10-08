b1f3b5884d038ebc235eea6d0489695a484338a3
shupakabras <shupakabras@gmail.com>
2018-10-09 01:42:10 +0300

    "Size - width" and "size - height" are working only if style is configured as default for a particular type

--- src/org/kdb/studio/actions/ChartAction.java
+++ src/org/kdb/studio/actions/ChartAction.java
@@ -7,6 +7,8 @@ import com.intellij.openapi.ui.WindowWrapperBuilder;
 import com.intellij.openapi.util.IconLoader;
 import org.kdb.studio.ui.LineChartForm;
 
+import javax.swing.*;
+
 public class ChartAction extends QGridAction {
 
     public ChartAction() {
@@ -19,12 +21,19 @@ public class ChartAction extends QGridAction {
         Project project = anActionEvent.getProject();
         if (isAvailable(anActionEvent) && getTableGroup(anActionEvent).getCurrentTableModel() != null) {
 
-            WindowWrapper wrapper = new WindowWrapperBuilder(WindowWrapper.Mode.FRAME, new LineChartForm(getTableGroup(anActionEvent).getCurrentTableModel()).createCenterPanel())
+            LineChartForm lineChartForm = new LineChartForm(getTableGroup(anActionEvent).getCurrentTableModel());
+            WindowWrapper wrapper = new WindowWrapperBuilder(WindowWrapper.Mode.FRAME, lineChartForm.createCenterPanel())
                     .setProject(project).setTitle("Chart").build();
             wrapper.getWindow().setSize(wrapper.getComponent().getPreferredSize());
             wrapper.getWindow().setLocationRelativeTo(null);
             wrapper.show();
-            //new LineChartForm(project, getTableGroup(anActionEvent).getCurrentTableModel()).show();
+            lineChartForm.addPreferredSizeChangeListener(dimension -> {
+                if (JFrame.class.isAssignableFrom(wrapper.getWindow().getClass())) {
+                    if (JFrame.class.cast(wrapper.getWindow()).getExtendedState() != JFrame.MAXIMIZED_BOTH) {
+                        wrapper.getWindow().setSize(dimension);
+                    }
+                }
+            });
         }
     }
 
