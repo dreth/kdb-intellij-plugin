62840f1ddbd950ac916823f0a35bfb9ad077a479
ddeliya <ddeliya@gmail.com>
2025-02-16 15:54:07 +0000

    Fixed one more warning "Toolbar actions that need local UI context would be incorrectly disabled". Fixed a warning about "Shall not be called on a template presentation".

--- src/main/java/org/kdb/studio/actions/PlotOverrideAction.java
+++ src/main/java/org/kdb/studio/actions/PlotOverrideAction.java
@@ -1,8 +1,6 @@
 package org.kdb.studio.actions;
 
 import com.intellij.icons.AllIcons;
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationListener;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.ui.MessageType;
@@ -10,10 +8,10 @@ import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.ui.BrowserHyperlinkListener;
 import com.intellij.ui.awt.RelativePoint;
-import org.kdb.studio.KDBResourceBundle;
+import com.intellij.util.ui.JBUI;
+import org.jetbrains.annotations.NotNull;
 import org.kdb.studio.chart.entity.PlotOverride;
 
-import javax.swing.*;
 import java.awt.*;
 
 public class PlotOverrideAction extends AnAction {
@@ -27,31 +25,42 @@ public class PlotOverrideAction extends AnAction {
     }
 
     @Override
-    public void actionPerformed(AnActionEvent anActionEvent) {
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
 
         if (lastState != null && lastState.messageType != null) {
             JBPopupFactory.getInstance()
                     .createHtmlTextBalloonBuilder(lastState.details, lastState.messageType, BrowserHyperlinkListener.INSTANCE)
                     .setAnimationCycle(200)
-                    .setBorderInsets(new Insets(5, 5, 5, 5))
+                    .setBorderInsets(JBUI.insets(5))
                     .createBalloon()
                     .show(new RelativePoint(component, new Point(15, 15)), Balloon.Position.below);
         }
     }
 
     public void setPlotOverride(PlotOverride override) {
+        // The thing returned by getTemplatePresentation() is immutable, do not apply "set*" methods on it.
         lastState = override;
-        if (override.messageType == MessageType.INFO) {
-            getTemplatePresentation().setVisible(true);
-            getTemplatePresentation().setIcon(AllIcons.General.BalloonInformation);
-            return;
-        }
-        if (override.messageType == MessageType.WARNING) {
-            getTemplatePresentation().setVisible(true);
-            getTemplatePresentation().setIcon(AllIcons.General.BalloonWarning);
-            return;
-        }
-        getTemplatePresentation().setIcon(null);
-        getTemplatePresentation().setVisible(false);
+        // Instead of updating the template, wait for IntelliJ action system to trigger update sometime later.
+        // There is now a small delay after a style is selected in the dropdown,
+        // and when the new selection appears as text. I will leave it as is for now.
     }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        if (lastState != null) {
+            if (lastState.messageType == MessageType.INFO) {
+                e.getPresentation().setVisible(true);
+                e.getPresentation().setIcon(AllIcons.General.BalloonInformation);
+                return;
+            } else if (lastState.messageType == MessageType.WARNING) {
+                e.getPresentation().setVisible(true);
+                e.getPresentation().setIcon(AllIcons.General.BalloonWarning);
+                return;
+            }
+        }
+        // Hide the action if there is no override, or if message type is not known.
+        e.getPresentation().setIcon(null);
+        e.getPresentation().setVisible(false);
+    }
+
 }
